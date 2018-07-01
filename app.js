59237d9aee31f889c9aac5ee5a87b8756d800482
LAP-YOYO\Mahmoud Kida <mahmoud@uxbert.com>
2018-07-01 23:48:39 +0300

    try supoorting heruko

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const apiai = require('apiai')('bccb47faa1144397aad612f4acf8f88e');
+var apiai = require('apiai')('bccb47faa1144397aad612f4acf8f88e');
 
 
 
@@ -11,46 +11,52 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var server  = require('http').createServer(app);
+var server = require('http').createServer(app);
 
-app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Credentials", "true");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-        next();
-    });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
 
-var io = require('socket.io')(server,{
-        origins: '*:*'
-        // 'transports': ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']
-    });
+var io = require('socket.io')(server, {
+    origins: '*:*'
+    // 'transports': ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']
+});
+
 server.listen(4200);
 
-io.on('connection', function(socket) {
-  socket.on('chat message', (text) => {
+io.configure(function () {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+});
 
-    // Get a reply from API.AI
-	console.log("client text: " + text);
+io.on('connection', function (socket) {
+    socket.on('chat message', (text) => {
 
-    let apiaiReq = apiai.textRequest(text, {
-      sessionId: Math.random() * 1000
+        // Get a reply from API.AI
+        console.log("client text: " + text);
+
+        let apiaiReq = apiai.textRequest(text, {
+            sessionId: Math.random() * 1000
+        });
+
+        apiaiReq.on('response', (response) => {
+            let aiText = response.result.fulfillment.speech;
+            console.log("bot text: " + aiText);
+            socket.emit('bot reply', aiText); // Send the result back to the browser!
+        });
+
+        apiaiReq.on('error', (error) => {
+            console.log(error);
+        });
+
+        apiaiReq.end();
+
     });
-
-    apiaiReq.on('response', (response) => {
-      let aiText = response.result.fulfillment.speech;
-	  console.log("bot text: " + aiText);
-      socket.emit('bot reply', aiText); // Send the result back to the browser!
-    });
-
-    apiaiReq.on('error', (error) => {
-      console.log(error);
-    });
-
-    apiaiReq.end();
-
-  });
 });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -58,7 +64,9 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -66,19 +74,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
